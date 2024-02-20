316b075f9b3bfe5aca05dffc75c01e8240c9a1d3
Jatin-dev12 <jatin@iosandweb.net>
2024-02-20 16:44:39 +0530

    Translator chla dia bhai

--- js/src/Pages/Ai.js
+++ js/src/Pages/Ai.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import axios from "axios";
 import FormData from "form-data";
 import "./Ai.css";
 import { Container, Row, Col } from "react-bootstrap";
-import supportedLanguages from "../Text.json";
 import Spellchecker from "hunspell-spellchecker";
 
 const Ai = () => {
@@ -25,6 +24,57 @@ const Ai = () => {
   const { transcript, resetTranscript, listening } = useSpeechRecognition({ language: currentLanguage });
   const [correctedText, setCorrectedText] = useState("");
   const [translation, setTranslation] = useState("");
+  const [fromText, setFromText] = useState("");
+  const [toText, setToText] = useState("");
+  const [translateFrom, setTranslateFrom] = useState("en-GB");
+  const [translateTo, setTranslateTo] = useState("hi-IN");
+  const [translationPlaceholder, setTranslationPlaceholder] = useState("Translation");
+  const countries = {
+    "am-ET": "Amharic",    "ar-SA": "Arabic",    "be-BY": "Bielarus",    "bem-ZM": "Bemba",    "bi-VU": "Bislama",    "bjs-BB": "Bajan",    "bn-IN": "Bengali",    "bo-CN": "Tibetan",    "br-FR": "Breton",    "bs-BA": "Bosnian",    "ca-ES": "Catalan",    "cop-EG": "Coptic",    "cs-CZ": "Czech",    "cy-GB": "Welsh",    "da-DK": "Danish",    "dz-BT": "Dzongkha",    "de-DE": "German",    "dv-MV": "Maldivian",
+    "el-GR": "Greek",    "en-GB": "English",    "es-ES": "Spanish",    "et-EE": "Estonian",
+    "eu-ES": "Basque",    "fa-IR": "Persian",    "fi-FI": "Finnish",    "fn-FNG": "Fanagalo",    "fo-FO": "Faroese",
+    "fr-FR": "French",    "gl-ES": "Galician",    "gu-IN": "Gujarati",    "ha-NE": "Hausa",
+    "he-IL": "Hebrew",    "hi-IN": "Hindi",    "hr-HR": "Croatian",    "hu-HU": "Hungarian",
+    "id-ID": "Indonesian",    "is-IS": "Icelandic",    "it-IT": "Italian",    "ja-JP": "Japanese",
+    "kk-KZ": "Kazakh",    "km-KM": "Khmer",    "kn-IN": "Kannada",    "ko-KR": "Korean",
+    "ku-TR": "Kurdish",    "ky-KG": "Kyrgyz",    "la-VA": "Latin",    "lo-LA": "Lao",    "lv-LV": "Latvian",
+    "men-SL": "Mende",    "mg-MG": "Malagasy",   "mi-NZ": "Maori",    "ms-MY": "Malay",    "mt-MT": "Maltese",
+    "my-MM": "Burmese",    "ne-NP": "Nepali",    "niu-NU": "Niuean",    "nl-NL": "Dutch",
+    "no-NO": "Norwegian",    "ny-MW": "Nyanja",    "ur-PK": "Pakistani",    "pau-PW": "Palauan",    "pa-IN": "Panjabi",
+    "ps-PK": "Pashto",   "pis-SB": "Pijin",    "pl-PL": "Polish",    "pt-PT": "Portuguese",    "rn-BI": "Kirundi",
+    "ro-RO": "Romanian",    "ru-RU": "Russian",    "sg-CF": "Sango",    "si-LK": "Sinhala",
+    "sk-SK": "Slovak",    "sm-WS": "Samoan",    "sn-ZW": "Shona",    "so-SO": "Somali",   "sq-AL": "Albanian",
+    "sr-RS": "Serbian",    "sv-SE": "Swedish",    "sw-SZ": "Swahili",    "ta-LK": "Tamil",
+    "te-IN": "Telugu",    "tet-TL": "Tetum",   "tg-TJ": "Tajik",    "th-TH": "Thai",    "ti-TI": "Tigrinya",
+    "tk-TM": "Turkmen",    "tl-PH": "Tagalog",    "tn-BW": "Tswana",    "to-TO": "Tongan",
+    "tr-TR": "Turkish",    "uk-UA": "Ukrainian",    "uz-UZ": "Uzbek",    "vi-VN": "Vietnamese",
+    "wo-SN": "Wolof",    "xh-ZA": "Xhosa",    "yi-YD": "Yiddish",    "zu-ZA": "Zulu"
+};
+useEffect(() => {
+  fetchTranslation();
+}, [transcript, translateFrom, translateTo]);
+
+const fetchTranslation = () => {
+  if (!transcript) return;
+  setTranslationPlaceholder("Translating...");
+  const apiUrl = `https://api.mymemory.translated.net/get?q=${transcript}&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl)
+  .then((res) => res.json())
+  .then((data) => {
+      const translatedText = data.responseData.translatedText;
+      setToText(translatedText    );
+      setTranslationPlaceholder("Translation");
+  });
+};
+const handleExchange = () => {
+  const tempText = fromText;
+  const tempLang = translateFrom;
+  setFromText(toText);
+  setToText(tempText);
+  setTranslateFrom(translateTo);
+  setTranslateTo(tempLang);
+};
+
 
   const startListening = () => {
     resetTranscript();
@@ -90,7 +140,13 @@ const Ai = () => {
         </p>
 
         <div className="main-content">
-        {transcript}
+        <textarea
+            className="from-text"
+            value={transcript}
+            onChange={(e) => setFromText(e.target.value)}
+            placeholder="SPEAK..."
+            />
+        {}
         </div>
 
         <Container>
@@ -102,6 +158,14 @@ const Ai = () => {
                   {language.name}
                 </option>
               ))}
+            
+            value={translateFrom} onChange={(e) => setTranslateFrom(e.target.value)}
+            {Object.entries(countries).map(([code, name]) => (
+                <option key={code} value={code}>
+                {name}
+                </option>
+            ))}
+            
             </select></Col></Row>
         <Row>   
           <Col></Col>  
@@ -109,6 +173,21 @@ const Ai = () => {
         <Col><button className="sss"   onClick={switchLanguage}>Switch Language</button></Col>
         <Col><button onClick={SpeechRecognition.stopListening}>Stop Listening</button></Col>   
         <Col></Col>
+        <Row>
+          <Col>
+          <h4> Select RealTime Trancription Language </h4>
+          
+           <select value={translateTo} onChange={(e) => setTranslateTo(e.target.value)}>
+            {Object.entries(countries).map(([code, name]) => (
+                <option key={code} value={code}>
+                {name}
+                </option>
+            ))}
+            </select></Col>
+       
+        <Col><textarea className="to-text" value={toText} readOnly placeholder={translationPlaceholder} />
+    </Col>
+        </Row>
           
           
           
--- js/src/Pages/Text.js
+++ js/src/Pages/Text.js
@@ -1,149 +1,112 @@
-// import React, { useState } from "react";
-// import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-// import axios from "axios";
-// import FormData from "form-data";
-// import "./Ai.css";
-// import { Container, Row, Col } from "react-bootstrap";
-// import supportedLanguages from "../Text.json";
-// import Spellchecker from "hunspell-spellchecker";
+    import React, { useState, useEffect } from "react";
+    import Container from 'react-bootstrap/Container';
+    import Row from 'react-bootstrap/Row';
+    import Col from 'react-bootstrap/Col';
 
-// // Set AssemblyAI Axios Header
-// const assembly = axios.create({
-//   baseURL: "https://api.assemblyai.com/v2",
-//   headers: {
-//     authorization: "04663cad0aec465abe74f41e7bb40f6e",
-//     "content-type": "application/json",
-//     "transfer-encoding": "chunked",
-//   },
-  
-// });
+    const TranslateApp = () => {
+    const [fromText, setFromText] = useState("");
+    const [toText, setToText] = useState("");
+    const [translateFrom, setTranslateFrom] = useState("en-GB");
+    const [translateTo, setTranslateTo] = useState("hi-IN");
+    const [translationPlaceholder, setTranslationPlaceholder] = useState("Translation");
 
-// const Ai = () => {
-//   const [supportedLanguages, setSupportedLanguages] = useState([
-//     { code: "sq", name: "Albanian" },
-//     { code: "bn", name: "Bengali" },
-//     { code: "fr", name: "French" },
-//     { code: "en", name: "English" },
-//     { code: "de", name: "German" },
-//     { code: "gu", name: "Gujarati" },
-//     { code: "ja", name: "Japanese" },
-//     { code: "hi", name: "Hindi" },
-//     { code: "ka", name: "Georgian" },
-//     { code: "ne", name: "Nepali" },
-//     { code: "ml", name: "Malayalam" },
-//     { code: "ta", name: "Tamil" },
-//     { code: "pa", name: "Punjabi" },
-//     { code: "ru", name: "Russian" },
-//     { code: "af", name: "Afrikaans" },
-//     { code: "fr", name: "French" },
-//     { code: "am", name: "Amharic" },
-//     { code: "ar", name: "Arabic" },
-//     { code: "hy", name: "Armenian" },
-//     { code: "az", name: "Azerbaijani" },
-//     { code: "eu", name: "Basque" },
-//     { code: "bs", name: "Bosnian" },
-//     { code: "bg", name: "Bulgarian" },
-//     { code: "ca", name: "Catalan" },
-//     { code: "ceb", name: "Cebuano" },
-//     { code: "ny", name: "Chichewa" },
-//     // ...
-//   ]);
-//   const [currentLanguage, setCurrentLanguage] = useState("en");
-//   const { transcript, resetTranscript, listening } = useSpeechRecognition({ language: currentLanguage });
-//   const [correctedText, setCorrectedText] = useState("");
-//   const [translation, setTranslation] = useState("");
+    const countries = {
+        "am-ET": "Amharic",    "ar-SA": "Arabic",    "be-BY": "Bielarus",    "bem-ZM": "Bemba",    "bi-VU": "Bislama",    "bjs-BB": "Bajan",    "bn-IN": "Bengali",    "bo-CN": "Tibetan",    "br-FR": "Breton",    "bs-BA": "Bosnian",    "ca-ES": "Catalan",    "cop-EG": "Coptic",    "cs-CZ": "Czech",    "cy-GB": "Welsh",    "da-DK": "Danish",    "dz-BT": "Dzongkha",    "de-DE": "German",    "dv-MV": "Maldivian",
+        "el-GR": "Greek",    "en-GB": "English",    "es-ES": "Spanish",    "et-EE": "Estonian",
+        "eu-ES": "Basque",    "fa-IR": "Persian",    "fi-FI": "Finnish",    "fn-FNG": "Fanagalo",    "fo-FO": "Faroese",
+        "fr-FR": "French",    "gl-ES": "Galician",    "gu-IN": "Gujarati",    "ha-NE": "Hausa",
+        "he-IL": "Hebrew",    "hi-IN": "Hindi",    "hr-HR": "Croatian",    "hu-HU": "Hungarian",
+        "id-ID": "Indonesian",    "is-IS": "Icelandic",    "it-IT": "Italian",    "ja-JP": "Japanese",
+        "kk-KZ": "Kazakh",    "km-KM": "Khmer",    "kn-IN": "Kannada",    "ko-KR": "Korean",
+        "ku-TR": "Kurdish",    "ky-KG": "Kyrgyz",    "la-VA": "Latin",    "lo-LA": "Lao",    "lv-LV": "Latvian",
+        "men-SL": "Mende",    "mg-MG": "Malagasy",   "mi-NZ": "Maori",    "ms-MY": "Malay",    "mt-MT": "Maltese",
+        "my-MM": "Burmese",    "ne-NP": "Nepali",    "niu-NU": "Niuean",    "nl-NL": "Dutch",
+        "no-NO": "Norwegian",    "ny-MW": "Nyanja",    "ur-PK": "Pakistani",    "pau-PW": "Palauan",    "pa-IN": "Panjabi",
+        "ps-PK": "Pashto",   "pis-SB": "Pijin",    "pl-PL": "Polish",    "pt-PT": "Portuguese",    "rn-BI": "Kirundi",
+        "ro-RO": "Romanian",    "ru-RU": "Russian",    "sg-CF": "Sango",    "si-LK": "Sinhala",
+        "sk-SK": "Slovak",    "sm-WS": "Samoan",    "sn-ZW": "Shona",    "so-SO": "Somali",   "sq-AL": "Albanian",
+        "sr-RS": "Serbian",    "sv-SE": "Swedish",    "sw-SZ": "Swahili",    "ta-LK": "Tamil",
+        "te-IN": "Telugu",    "tet-TL": "Tetum",   "tg-TJ": "Tajik",    "th-TH": "Thai",    "ti-TI": "Tigrinya",
+        "tk-TM": "Turkmen",    "tl-PH": "Tagalog",    "tn-BW": "Tswana",    "to-TO": "Tongan",
+        "tr-TR": "Turkish",    "uk-UA": "Ukrainian",    "uz-UZ": "Uzbek",    "vi-VN": "Vietnamese",
+        "wo-SN": "Wolof",    "xh-ZA": "Xhosa",    "yi-YD": "Yiddish",    "zu-ZA": "Zulu"
+    };
 
-//   const startListening = () => {
-//     resetTranscript();
-//     SpeechRecognition.startListening({ continuous: true, language: currentLanguage });
-//   };
+    useEffect(() => {
+        fetchTranslation();
+    }, [fromText, translateFrom, translateTo]);
 
-//   const switchLanguage = () => {
-//     const index = supportedLanguages.findIndex((lang) => lang.code === currentLanguage);
-//     const newLanguage =
-//       index === supportedLanguages.length - 1
-//         ? supportedLanguages[0].code
-//         : supportedLanguages[index + 1].code;
-//     setCurrentLanguage(newLanguage);
-//   };
+    const fetchTranslation = () => {
+        if (!fromText) return;
+        setTranslationPlaceholder("Translating...");
+        const apiUrl = `https://api.mymemory.translated.net/get?q=${fromText}&langpair=${translateFrom}|${translateTo}`;
+        fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            const translatedText = data.responseData.translatedText;
+            setToText(translatedText    );
+            setTranslationPlaceholder("Translation");
+        });
+    };
 
-//   const correctSpelling = (text) => {
-//     const spellchecker = new Spellchecker("en_US");
-//     const suggestions = spellchecker.getSuggestions(text);
-//     if (suggestions.length > 0) {
-//       return suggestions[0];
-//     }
-//     return text;
-//   };
+    const handleExchange = () => {
+        const tempText = fromText;
+        const tempLang = translateFrom;
+        setFromText(toText);
+        setToText(tempText);
+        setTranslateFrom(translateTo);
+        setTranslateTo(tempLang);
+    };
 
+    const handleCopy = (text) => {
+        navigator.clipboard.writeText(text);
+    };
 
+    const handleSpeak = (text, lang) => {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = lang;
+        speechSynthesis.speak(utterance);
+    };
 
-//   const handleTranscription = async () => {
-//     const truncatedTranscript = transcript.split(" ").slice(0, 100).join(" ");
-//     let correctedText = truncatedTranscript.split(" ").map(correctSpelling).join(" ");
-//     setCorrectedText(correctedText);
+    return (
+        <Container className="lsjd">   
+            <Row>
+                <Col >
+                <textarea
+            className="from-text"
+            value={fromText}
+            onChange={(e) => setFromText(e.target.value)}
+            placeholder="Enter text to translate"
+            /></Col>
 
-//     if (correctedText.length > 0) {
-//       // upload the audio file to AssemblyAI
-//       const formData = new FormData();
-//       formData.append("file", new Blob([Buffer.from(correctedText)], { type: "audio/mpeg-3" }));
+                <Col><textarea className="to-text" value={toText} readOnly placeholder={translationPlaceholder} />
+    </Col>
+        </Row>
+        <Row>
+        <Col >
+            <select value={translateFrom} onChange={(e) => setTranslateFrom(e.target.value)}>
+            {Object.entries(countries).map(([code, name]) => (
+                <option key={code} value={code}>
+                {name}
+                </option>
+            ))}
+            
+            </select>
+            </Col>
+            <Col><select value={translateTo} onChange={(e) => setTranslateTo(e.target.value)}>
+            {Object.entries(countries).map(([code, name]) => (
+                <option key={code} value={code}>
+                {name}
+                </option>
+            ))}
+            </select></Col>
+            </Row> 
+            <button onClick={fetchTranslation}>Translate</button>
 
-//       try {
-//         const response = await assembly.post("/transcript", formData, {
-//           headers: {
-//             ...formData.getHeaders(),
-//           },
-//         });
+        
+        </Container>
+    
+    );
+    };
 
-//         // do something with the response, like setting the translation state
-//         setTranslation(response.data.text);
-//       } catch (error) {
-//         console.error("Error during transcription:", error);
-//       }
-//     }
-//     await transcriber.connect()
-//   };
-
-//   return (
-//     <>
-//       <div className="containers">
-//         <h2>Speech to Text Converter</h2>
-
-//         <p className="sa">
-//           What Ever You Speak It Will Write Here Let's Say Something.
-//         </p>
-
-//         <div className="main-content">
-//         {transcript}
-//         </div>
-
-//         <Container>
-//           <Row>
-//             <Col>
-//               <select value={currentLanguage} onChange={(e) => setCurrentLanguage(e.target.value)}>
-//                 {supportedLanguages.map((language) => (
-//                   <option key={language.code} value={language.code}>
-//                   {language.name}
-//                 </option>
-//               ))}
-//             </select></Col></Row>
-//         <Row>   
-//           <Col></Col>  
-//         <Col><button onClick={startListening}>Start Listening</button></Col>
-//         <Col><button className="sss"   onClick={switchLanguage}>Switch Language</button></Col>
-//         <Col><button onClick={SpeechRecognition.stopListening}>Stop Listening</button></Col>   
-//         <Col></Col>
-          
-          
-          
-
-          
-           
-//             </Row>
-//          </Container>   
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Ai;
+    export default TranslateApp;
